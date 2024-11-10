3d292011cdd138bf4d61667974d3501f7967c8e4
Multiprocesssing-CODER <sagnik.dey745@icloud.com>
2024-11-11 05:13:29 +0530

    New Budget section Created

--- components/CreateBudget.jsx
+++ components/CreateBudget.jsx
@@ -37,73 +37,70 @@ const CreateBudget = () => {
         icon: emojiIcon,
       })
       .returning({ insertedId: Budgets.id });
-    console.log(result);
     if (result) {
       toast("New Budget Created!");
     }
   };
   return (
-    <div>
-      <Dialog>
-        <DialogTrigger>
-          <div className="bg-slate-100 p-10 rounded-md items-center flex flex-col border-2 border-dashed cursor-pointer hover:shadow-md">
-            <h2 className="text-3xl">+</h2>
-            <h2>Create New Budget</h2>
-          </div>
-        </DialogTrigger>
-        <DialogContent>
-          <DialogHeader>
-            <DialogTitle>Create New Budget</DialogTitle>
-            <DialogDescription></DialogDescription>
-          </DialogHeader>
-          <div className="mt-5 ">
+    <Dialog>
+      <DialogTrigger>
+        <div className="bg-slate-100 p-10 rounded-2xl items-center flex flex-col border-2 border-dashed cursor-pointer hover:shadow-md">
+          <h2 className="text-3xl">+</h2>
+          <h2>Create New Budget</h2>
+        </div>
+      </DialogTrigger>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>Create New Budget</DialogTitle>
+          <DialogDescription></DialogDescription>
+        </DialogHeader>
+        <div className="mt-5 ">
+          <Button
+            variant="outline"
+            size="lg"
+            onClick={() => setOpenEmojiPicker(!openEmojiPicker)}
+          >
+            {emojiIcon}
+          </Button>
+        </div>
+        <div className="relative">
+          <EmojiPicker
+            open={openEmojiPicker}
+            onEmojiClick={(e) => {
+              setEmojiIcon(e.emoji);
+              setOpenEmojiPicker(false);
+            }}
+          />
+        </div>
+        <div className="mt-2">
+          <h2 className="text-black font-medium my-1">Budget Name</h2>
+          <Input
+            type="text"
+            placeholder="e.g.Home Decor"
+            onChange={(e) => setname(e.target.value)}
+          />
+        </div>
+        <div className="mt-2">
+          <h2 className="text-black font-medium my-1">Budget Amount</h2>
+          <Input
+            type="number"
+            placeholder="e.g.Rs.5000"
+            onChange={(e) => setamount(e.target.value)}
+          />
+        </div>
+        <DialogFooter className="sm:justify-start">
+          <DialogClose asChild>
             <Button
-              variant="outline"
-              size="lg"
-              onClick={() => setOpenEmojiPicker(!openEmojiPicker)}
+              className="mt-5 w-full rounded-2xl"
+              onClick={() => onCreateBudget()}
+              disabled={!(name && amount)}
             >
-              {emojiIcon}
+              Create Budget
             </Button>
-          </div>
-          <div className="relative">
-            <EmojiPicker
-              open={openEmojiPicker}
-              onEmojiClick={(e) => {
-                setEmojiIcon(e.emoji);
-                setOpenEmojiPicker(false);
-              }}
-            />
-          </div>
-          <div className="mt-2">
-            <h2 className="text-black font-medium my-1">Budget Name</h2>
-            <Input
-              type="text"
-              placeholder="e.g.Home Decor"
-              onChange={(e) => setname(e.target.value)}
-            />
-          </div>
-          <div className="mt-2">
-            <h2 className="text-black font-medium my-1">Budget Amount</h2>
-            <Input
-              type="number"
-              placeholder="e.g.Rs.5000"
-              onChange={(e) => setamount(e.target.value)}
-            />
-          </div>
-          <DialogFooter className="sm:justify-start">
-            <DialogClose asChild>
-              <Button
-                className="mt-5 w-full rounded-2xl"
-                onClick={() => onCreateBudget()}
-                disabled={!(name && amount)}
-              >
-                Create Budget
-              </Button>
-            </DialogClose>
-          </DialogFooter>
-        </DialogContent>
-      </Dialog>
-    </div>
+          </DialogClose>
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
   );
 };
 
