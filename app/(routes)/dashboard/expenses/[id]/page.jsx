f4be45f8caddb6d8dfedd9db90d638728ff697ee
Multiprocesssing-CODER <sagnik.dey745@icloud.com>
2024-11-13 02:56:43 +0530

    Added over budget Checking funtionality to avoid going over any specified budget and showing the alert

--- app/(routes)/dashboard/expenses/[id]/page.jsx
+++ app/(routes)/dashboard/expenses/[id]/page.jsx
@@ -3,12 +3,27 @@
 import AddExpense from "@/components/AddExpense";
 import BudgetItem from "@/components/BudgetItem";
 import ExpenseTable from "@/components/ExpenseTable";
+import { Button } from "@/components/ui/button";
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/components/ui/alert-dialog";
 import { Skeleton } from "@/components/ui/skeleton";
 import { db } from "@/utils/dbConfig";
 import { Budgets, Expenses } from "@/utils/schema";
 import { useUser } from "@clerk/nextjs";
 import { desc, eq, getTableColumns, sql } from "drizzle-orm";
+import { Trash2 } from "lucide-react";
 import React, { useEffect, useState } from "react";
+import { toast } from "sonner";
+import { useRouter } from "next/navigation";
 
 const ExpensesDashboard = ({ params }) => {
   const { user } = useUser();
@@ -18,7 +33,7 @@ const ExpensesDashboard = ({ params }) => {
     user && getBudgetInfo();
     getListOfExpenses();
   }, [user]);
-
+  const route = useRouter();
   const getBudgetInfo = async () => {
     const result = await db
       .select({
@@ -31,8 +46,8 @@ const ExpensesDashboard = ({ params }) => {
       .where(eq(Budgets.createdBy, user?.primaryEmailAddress?.emailAddress))
       .where(eq(Budgets.id, params.id))
       .groupBy(Budgets.id);
-      getListOfExpenses();
-      setBudgetInfo(result[0]);
+    getListOfExpenses();
+    setBudgetInfo(result[0]);
   };
 
   const getListOfExpenses = async () => {
@@ -41,12 +56,54 @@ const ExpensesDashboard = ({ params }) => {
       .from(Expenses)
       .where(eq(Expenses.budgetId, params.id))
       .orderBy(desc(Expenses.id));
-      setExpensesList(result)
+    setExpensesList(result);
   };
+
+  const deleteBudget=async ()=>{
+    const deleteExpenseItems = await db
+    .delete(Expenses)
+    .where(eq(Expenses.budgetId, params.id))
+    .returning();
+
+    if(deleteExpenseItems) {
+      const result = await db
+        .delete(Budgets)
+        .where(eq(Budgets.id, params.id))
+        .returning();
+    }
+    toast(`Budget "${budgetInfo.name}" has been deleted!`);
+    route.replace("/dashboard/budgets");
+  }
   return (
     <div className="p-10 bg-gradient-to-b from-white via-blue-50 to-indigo-50 rounded-3xl shadow-2xl">
-      <h2 className="text-3xl font-bold text-transparent bg-clip-text bg-gradient-to-bl from-purple-600 to-yellow-500 via-blue-500 mb-4">
+      <h2 className="flex justify-between text-3xl font-bold text-transparent bg-clip-text bg-gradient-to-bl from-purple-600 to-yellow-500 via-blue-500 mb-4">
         My Expenses
+        <span>
+          <AlertDialog>
+            <AlertDialogTrigger asChild>
+              <Button variant="destructive" className="flex gap-2">
+                <Trash2 />
+                Delete
+              </Button>
+            </AlertDialogTrigger>
+            <AlertDialogContent>
+              <AlertDialogHeader>
+                <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
+                <AlertDialogDescription>
+                  This action cannot be undone. This will permanently delete
+                  your current budget "{budgetInfo ? budgetInfo.name : ""}" along
+                  with all its expenses and remove your data from the servers.
+                </AlertDialogDescription>
+              </AlertDialogHeader>
+              <AlertDialogFooter>
+                <AlertDialogCancel>Cancel</AlertDialogCancel>
+                <AlertDialogAction onClick={() => deleteBudget()}>
+                  Continue
+                </AlertDialogAction>
+              </AlertDialogFooter>
+            </AlertDialogContent>
+          </AlertDialog>
+        </span>
       </h2>
       {/* Budget Item and Skeleton Placeholder */}
       <div className="grid grid-cols-1 md:grid-cols-2 mt-6 gap-5">
@@ -57,6 +114,7 @@ const ExpensesDashboard = ({ params }) => {
         )}
         <AddExpense
           budgetId={params.id}
+          budgetAmount={budgetInfo? budgetInfo.amount : 0}
           user={user}
           refreshData={() => getBudgetInfo()}
         />
--- components/AddExpense.jsx
+++ components/AddExpense.jsx
@@ -1,30 +1,76 @@
 "use client";
 
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 import { Input } from "./ui/input";
 import { Button } from "./ui/button";
 import { db } from "@/utils/dbConfig";
 import { Budgets, Expenses } from "@/utils/schema";
 import { toast } from "sonner";
 import moment from "moment";
+import { eq, getTableColumns, sql } from "drizzle-orm";
+import { useUser } from "@clerk/nextjs";
+import { AlertCircle } from "lucide-react";
+import { Alert, AlertDescription, AlertTitle } from "./ui/alert";
 
-const AddExpense = ({ budgetId, refreshData }) => {
+const AddExpense = ({ budgetId, refreshData, budgetAmount }) => {
+  const {user } = useUser();
   const [name, setname] = useState();
   const [amount, setamount] = useState();
+  const [overBudget, setOverBudget] = useState(false);
+  const [overBudgetAmount, setOverBudgetAmount] = useState(0);
+  const alertTimeoutRef = useRef(null);
+
+  // Function to fetch the total expenses for the specified budget
+  const fetchTotalExpenses = async () => {
+    const result = await db
+      .select({
+        ...getTableColumns(Budgets),
+        totalSpend: sql`sum(${Expenses.amount})`.mapWith(Number),
+      })
+      .from(Budgets)
+      .leftJoin(Expenses, eq(Budgets.id, Expenses.budgetId))
+      .where(eq(Budgets.createdBy, user?.primaryEmailAddress?.emailAddress))
+      .where(eq(Budgets.id, budgetId))
+      .groupBy(Budgets.id);
+
+    return result[0].totalSpend ?? 0;
+  };
+
   const addNewExpense = async () => {
+    // Fetch the current total expenses
+    const currentTotal = await fetchTotalExpenses();
+    const newTotal = currentTotal + parseFloat(amount);
+
+    // Check if adding this expense will exceed the budget
+    if (newTotal > budgetAmount) {
+      setOverBudget(true);
+      setOverBudgetAmount(newTotal - budgetAmount);
+
+      // Clear any existing timeout to avoid conflicts
+      if (alertTimeoutRef.current) clearTimeout(alertTimeoutRef.current);
+
+      // Set timeout to hide alert after 3 seconds
+      alertTimeoutRef.current = setTimeout(() => {
+        setOverBudget(false);
+        setOverBudgetAmount(0);
+      }, 3000);
+
+      return;
+    }
+    console.log(newTotal);
     const result = await db
       .insert(Expenses)
       .values({
         name: name,
         amount: amount,
         budgetId: budgetId,
-        createdAt: moment().format('DD/MM/yyy'),
+        createdAt: moment().format("DD/MM/yyy"),
       })
       .returning({ insertedId: Budgets.id });
 
     if (result) {
-        refreshData()
-        toast("New Expense Added");
+      refreshData();
+      toast("New Expense Added");
     }
   };
   return (
@@ -32,6 +78,18 @@ const AddExpense = ({ budgetId, refreshData }) => {
       <h2 className="font-bold text-xl text-transparent bg-clip-text bg-gradient-to-r from-blue-500 via-purple-500 to-pink-500">
         Add Expense
       </h2>
+      {overBudget && (
+        <Alert variant="destructive" className="mt-4">
+          <AlertCircle className="h-4 w-4 mr-2" />
+          <div>
+            <AlertTitle>Error - Going Over Budget</AlertTitle>
+            <AlertDescription>
+              Adding this expense "{name}" will exceed your budget limit by Rs.
+              {overBudgetAmount}!
+            </AlertDescription>
+          </div>
+        </Alert>
+      )}
       <div className="mt-4">
         <h3 className="text-gray-700 font-medium my-1">Expense Name</h3>
         <Input
